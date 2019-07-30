81d7f332495abd04983fd2c8c70be15ade36c3fa
pookmish <pookmish@stanford.edu>
2019-07-30 14:31:43 -0700

    Update ExampleTest.php

--- tests/phpunit/ExampleTest.php
+++ tests/phpunit/ExampleTest.php
@@ -1,14 +1,18 @@
-
 <?php
+
 namespace My\Example\Project\Tests;
+
+use Drupal\Tests\UnitTestCase;
+
 /**
  * Class ExampleTest.
  */
-class ExampleTest extends \PHPUnit_Framework_TestCase {
+class ExampleTest extends UnitTestCase {
   /**
    * Asserts that universal laws are constant.
    */
   public function testExample() {
     $this->assertEquals(1, 1, "One equals one!");
   }
+  
 }
