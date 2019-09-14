6ef52a01eccd19c0497788bdcb5935e01cc66669
Mike Decker <pookmish@gmail.com>
2019-09-13 17:25:50 -0700

    phpcs cleanup

--- tests/src/Unit/ExampleTest.php
+++ tests/src/Unit/ExampleTest.php
@@ -8,11 +8,12 @@ use Drupal\Tests\UnitTestCase;
  * Class ExampleTest.
  */
 class ExampleTest extends UnitTestCase {
+
   /**
    * Asserts that universal laws are constant.
    */
   public function testExample() {
     $this->assertEquals(1, 1, "One equals one!");
   }
-  
+
 }
