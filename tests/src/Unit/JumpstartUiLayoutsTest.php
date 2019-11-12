60ffdde6d7ea5b64c6698e2ea4346be20d326696
Mike Decker <pookmish@gmail.com>
2019-11-11 16:10:56 -0800

    added test coverage

---
+++ tests/src/Unit/JumpstartUiLayoutsTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\Tests\jumpstart_ui\Unit;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\Form\FormState;
+use Drupal\jumpstart_ui\JumpstartUiLayouts;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Class JumpstartUiLayoutsTest.
+ *
+ * @group jumpstart_ui
+ * @coversDefaultClass \Drupal\jumpstart_ui\JumpstartUiLayouts
+ */
+class JumpstartUiLayoutsTest extends UnitTestCase {
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $container = new ContainerBuilder();
+    $container->set('string_translation', $this->getStringTranslationStub());
+    \Drupal::setContainer($container);
+  }
+
+  /**
+   * The form class should save the values appropriately.
+   */
+  public function testLayoutForm() {
+    $object = new JumpstartUiLayouts([], '', []);
+    $this->assertArrayHasKey('extra_classes', $object->defaultConfiguration());
+    $this->assertArrayHasKey('centered', $object->defaultConfiguration());
+
+    $form = [];
+    $form_state = new FormState();
+    $form = $object->buildConfigurationForm($form, $form_state);
+
+    $this->assertArrayHasKey('extra_classes', $form);
+    $this->assertArrayHasKey('centered', $form);
+
+    $form_state->setValue('extra_classes', 'foo bar_baz');
+    $form_state->setValue('centered', FALSE);
+
+    $object->submitConfigurationForm($form, $form_state);
+    $config = $object->getConfiguration();
+
+    $this->assertEquals('foo bar-baz', $config['extra_classes']);
+    $this->assertNull($config['centered']);
+
+    $form_state->setValue('extra_classes', '@!:fooBar?');
+    $form_state->setValue('centered', TRUE);
+
+    $object->submitConfigurationForm($form, $form_state);
+    $config = $object->getConfiguration();
+
+    $this->assertEquals('fooBar', $config['extra_classes']);
+    $this->assertEquals('centered-container', $config['centered']);
+  }
+
+}
