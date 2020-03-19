35623314240c990e985c1ff8c4076e720acd76aa
Shea McKinney <sherakama@gmail.com>
2020-03-19 13:50:32 -0700

    Perhaps this makes sense

--- tests/src/Kernel/Pattern/PatternMediaTest.php
+++ tests/src/Kernel/Pattern/PatternMediaTest.php
@@ -5,6 +5,8 @@ namespace Drupal\Tests\jumpstart_ui\Kernel\Pattern;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\Core\Template\Attribute;
 use Twig\TemplateWrapper;
+use Drupal\ui_patterns\Element\PatternContext;
+use Drupal\Component\Utility\Html;
 
 /**
  * Class PatternMediaTest.
@@ -68,23 +70,46 @@ class PatternMediaTest extends KernelTestBase {
    * Pattern should not produce duplicate ids.
    */
   public function testMediaPatternIds() {
-    $props = [
-      'attributes' => new Attribute(['class' => 'su-media', 'id' => 'su-media']),
-      'media_caption' => 'You must do the things you think you cannot do.',
-      'media_custom' => 'Nothing to see here. Please move along',
-    ];
     $template = drupal_get_path('module', 'jumpstart_ui') . "/templates/components/media/media.html.twig";
+    $props = $this->getProps();
     $this->setRawContent((string) twig_render_template($template, $props));
 
     $this->assertText("You must do the things you think you cannot do");
     $this->assertText("Nothing to see here");
     $this->assertContains("id=\"su-media\"", $this->getRawContent());
 
+    $props = $this->getProps();
     $this->setRawContent((string) twig_render_template($template, $props));
     $this->assertContains("id=\"su-media--2\"", $this->getRawContent());
 
+    $props = $this->getProps();
     $this->setRawContent((string) twig_render_template($template, $props));
     $this->assertContains("id=\"su-media--3\"", $this->getRawContent());
+  }
+
+  /**
+   * [getProps description]
+   * @return [type] [description]
+   */
+  protected function getProps() {
+    $props = [
+      'attributes' => ['class' => 'su-media', 'id' => 'su-media'],
+      'media_caption' => 'You must do the things you think you cannot do.',
+      'media_custom' => 'Nothing to see here. Please move along',
+      'context' => new PatternContext('empty'),
+    ];
+    $this->fakeJumpstartUIPreproces($props, 'pattern_media');
+    $props['attributes'] = new Attribute($props['attributes']);
+    return $props;
+  }
+
+  /**
+   * fake parts of the preprocess function.
+   */
+  protected function fakeJumpstartUIPreproces(&$variables, $hook) {
+    if (isset($variables['attributes']['id'])) {
+      $variables['attributes']['id'] = Html::getUniqueId($variables['attributes']['id']);
+    }
   }
 
 }
