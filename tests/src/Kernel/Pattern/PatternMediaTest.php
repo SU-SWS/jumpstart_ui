237a7ece2c30a3e91d0337a8e45eb15742bd1c38
Shea McKinney <sherakama@gmail.com>
2020-03-18 23:25:37 -0700

    D8CORE-1504: Added tests to validate namespaces and ids.

--- tests/src/Kernel/Pattern/PatternMediaTest.php
+++ tests/src/Kernel/Pattern/PatternMediaTest.php
@@ -6,6 +6,7 @@ use Drupal\KernelTests\KernelTestBase;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Drupal\Core\Template\Attribute;
+use Twig\TemplateWrapper;
 
 /**
  * Class PatternMediaTest.
@@ -27,57 +28,65 @@ class PatternMediaTest extends KernelTestBase {
     'ui_patterns',
     'ui_patterns_ds',
     'node',
-    'user'
+    'user',
   ];
 
-  public function setup() {
-    parent::setup();
-    // $this->installSchema('components', 'settings');
+  /**
+   * @var \Drupal\Core\Template\TwigEnvironment
+   */
+  protected $twig;
+
+  /**
+   * Setup.
+   */
+  protected function setUp() {
+    parent::setUp();
+    \Drupal::service('theme_installer')->install(['bartik']);
+    $this->twig = \Drupal::service('twig');
   }
 
   /**
-   * {@inheritdoc}
+   * Checks to see if a value is a twig template.
    */
-  public function register(ContainerBuilder $container) {
-    parent::register($container);
+  public function assertTwigTemplate($value, $message = '', $group = 'Other') {
+    $this->assertTrue($value instanceof TemplateWrapper, $message, $group);
+  }
 
-    $container->setDefinition('twig_loader__file_system', new Definition('Twig_Loader_Filesystem',
-      [
-        [
-          dirname(__FILE__, 5) . '/templates/components/',
-          dirname(__FILE__, 5) . '/dist/templates/'
-        ]
-      ]
-    ))
-      ->addTag('twig.loader');
+  /**
+   * Check to see if twig namespaces are working.
+   */
+  public function testTwigNamespaces() {
+    // Tests resolving namespaced templates in modules.
+    $this->assertTwigTemplate($this->twig->load('@node/node.html.twig'), 'Found node.html.twig in node module.');
+
+    // Tests resolving namespaced templates in jumpstart_ui.
+    $this->assertTwigTemplate($this->twig->load('@jumpstart_ui/components/card/card.html.twig'), 'Found card.html.twig in jumpstart_ui module.');
+
+    // Tests resolving namespaced templates in the Decanter templates.
+    $this->assertTwigTemplate($this->twig->load('@decanter/components/card/card.twig'), 'Found card.html.twig in jumpstart_ui module.');
   }
 
   /**
    * Pattern should not produce duplicate ids.
    */
   public function testMediaPatternIds() {
-    // Boot twig environment.
-    $twig =  \Drupal::service('twig');
-    $template = drupal_get_path('module', 'jumpstart_ui') . '/templates/components/media/media.html.twig';
     $props = [
-      'media_caption' => ["Well, hello there. I am a caption."],
-      'media_custom' => ['<div class="media">Nothing to see here, please move along.</div>'],
-      'attributes' => new Attribute(['class' => 'su-media', 'id' => 'su-media']),
+      'attributes' => new Attribute(['class' => 'su-media', 'id' => 'media']),
+      'media_caption' => 'You must do the things you think you cannot do.',
+      'media_custom' => 'Nothing to see here. Please move along',
     ];
+    $template = drupal_get_path('module', 'jumpstart_ui') . "/templates/components/media/media.html.twig";
     $this->setRawContent((string) twig_render_template($template, $props));
-    $this->assertText("Well, hello there. I am a caption.");
-    $this->assertContains("https://placeimg.com/2000/1333/any", $this->getRawContent());
+
+    $this->assertText("You must do the things you think you cannot do");
+    $this->assertText("Nothing to see here");
     $this->assertContains("id=\"su-media\"", $this->getRawContent());
 
     $this->setRawContent((string) twig_render_template($template, $props));
-    $this->assertText("Well, hello there. I am a caption.");
-    $this->assertContains("https://placeimg.com/2000/1333/any", $this->getRawContent());
     $this->assertContains("id=\"su-media--2\"", $this->getRawContent());
 
     $this->setRawContent((string) twig_render_template($template, $props));
-    $this->assertText("Well, hello there. I am a caption.");
-    $this->assertContains("https://placeimg.com/2000/1333/any", $this->getRawContent());
-    $this->assertContains("id=\"su-media--2\"", $this->getRawContent());
+    $this->assertContains("id=\"su-media--3\"", $this->getRawContent());
   }
 
 }
